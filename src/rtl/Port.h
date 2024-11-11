f94ac8a093480c7f6891bb87a2fa18345e41a609
Mike Reznikov <mirekez@outlook.com>
2024-11-11 23:51:24 +0100

    building connections between cells in RTL

--- src/rtl/Port.h
+++ src/rtl/Port.h
@@ -10,13 +10,12 @@ struct Module;
 struct Port
 {
     std::string name;
-    int width = -1;
     enum {
       PORT_IN,
       PORT_OUT,
       PORT_IO,
     } type;
-    std::vector<int> designators;
+    int designator;
 //    Ref<Module> module;
 };
 
