644fc641c960c2388320f56604acb326d355368a
Mike Reznikov <mirekez@outlook.com>
2024-12-22 23:38:13 +0100

    placing bunches stats calculated

--- src/tech/Tech.h
+++ src/tech/Tech.h
@@ -1,16 +1,43 @@
 #pragma once
 
-#include "Design.h"
-#include "Clocks.h"
-#include "Timings.h"
-#include "Placing.h"
+#include "debug.h"
+
+#include <vector>
+#include <string>
+#include <map>
+
+namespace tech
+{
+
+struct CombDelays
+{
+    std::map<std::string,std::pair<int,std::vector<double>>> map;
+
+    double getDelay(const std::string& type, int index_in, int index_out)
+    {
+        auto it = map.find(type);
+        if (it != map.end()) {
+            if (index_in < 0 || index_out < 0) {
+                PNR_WARNING("internal error: port index is not initialized at '{}'", type);
+            }
+            unsigned index = index_out*it->second.first/*portcnt*/ + index_in;
+            if (index < it->second.second.size()) {
+                PNR_LOG3("CLKT", " '{}':{}/{}={}/{}:{:.3f}", type,
+                    index_in, index_out, index, it->second.second.size(), it->second.second[index]);
+                }
+                return it->second.second[index];
+            }
+
+        return 0;
+    }
+};
 
 struct Tech
 {
-
-    rtl::Design design;
-    clk::Clocks clocks;
-    clk::Timings timings;
-    pnr::Placing placing;
-
+    static CombDelays comb_delays;
+    static std::multimap<std::string,std::string> clocked_ports;
+    static std::multimap<std::string,std::string> buffers_ports;
 };
+
+
+}
