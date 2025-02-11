7e71c12694a8676de3fccf7de1f918a400807747
Mike Reznikov <mirekez@outlook.com>
2025-02-11 22:38:05 +0100

    estimte design, outline design introduced

--- src/pnr/estimate/EstimateDesign.h
+++ src/pnr/estimate/EstimateDesign.h
@@ -13,33 +13,21 @@
 namespace pnr
 {
 
-struct Placing
+struct EstimateDesign
 {
-    struct DataOut
-    {
-        rtl::Inst* reg;
-        Referable<RegBunch> bunch;
-    };
-
-    std::vector<DataOut> data_outs;
+    std::list<Referable<RegBunch>> data_outs;
     int travers_mark = -1;
     tech::Tech* tech = nullptr;
     clk::Clocks* clocks = nullptr;
 
     void findTopOutputs(rtl::Design& rtl);
 
+    void estimateDesign(rtl::Design& rtl);
+    bool recurseReg(Referable<RegBunch>* bunch, rtl::Inst* reg, int depth = 0, int depth_comb = 0);
     void recurseComb(Referable<RegBunch>* bunch, rtl::Inst* comb, rtl::Conn* from, int depth = 0, int depth_comb = 0, double bottom_delay = 0, bool capture = false);
-    void recurseReg(Referable<RegBunch>* bunch, rtl::Inst* reg, int depth = 0, int depth_comb = 0);
-
-    void calculateDesign(rtl::Design& rtl)
-    {
-        findTopOutputs(rtl);
-        travers_mark = rtl::Inst::genMark();
-        for (auto& data_out : data_outs) {
-            data_out.bunch.reg = data_out.reg;
-            recurseReg(&data_out.bunch, data_out.bunch.reg);
-        }
-    }
+    int aggregateRegs(Referable<RegBunch>* bunch, int depth = 0, int count_empty = 0);
+    void sortBunches(std::list<Referable<RegBunch>>* bunch_list, int depth = 0);
+    void printBunches(std::list<Referable<RegBunch>>* bunch_list = nullptr, int depth = 0);
 
 //    void packBunch(Inst* reg, TileSet& tiles, int depth = 0);
 };
